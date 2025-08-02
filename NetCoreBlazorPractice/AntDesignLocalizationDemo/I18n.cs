5a94e596d0436ed34da2b0cd92e91389e940c2d3
菜鸟起飞 <734290300@qq.com>
2025-08-02 11:51:38 +0800

    中英切换demo

---
+++ NetCoreBlazorPractice/AntDesignLocalizationDemo/I18n.cs
@@ -0,0 +1,10 @@
+﻿namespace AntDesignLocalizationDemo
+{
+    // I18n.cs
+    // 这个类必须是 public，并且没有命名空间或在正确的命名空间下。
+    // 为了简单，我们使用全局命名空间。
+    public class I18n
+    {
+        // 空类，仅作为 .resx 文件的定位器
+    }
+}
---
+++ NetCoreBlazorPractice/AntDesignLocalizationDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using AntDesign.ProLayout;
+using Microsoft.AspNetCore.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddAntDesign();
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(sp.GetService<NavigationManager>()!.BaseUri)
+});
+builder.Services.Configure<ProSettings>(builder.Configuration.GetSection("ProSettings"));
+// 🔑 关键：添加交互式本地化服务
+// 注意：AddInteractiveStringLocalizer 会处理 AddLocalization，所以这里可以省略 AddLocalization
+builder.Services.AddInteractiveStringLocalizer();
+// 指定 .resx 文件所在的文件夹
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
